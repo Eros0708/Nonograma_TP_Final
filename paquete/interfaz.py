b70b91b55c0a08b61c0b8143f032f09268a690d0
Eros Menna <eroslorenzomenna1@gmail.com>
2025-11-26 23:33:05 -0300

    actualizacion

--- paquete/interfaz.py
+++ paquete/interfaz.py
@@ -1,20 +1,58 @@
 import pygame
-from config import *
+from paquete.config import *
+
 
 def render_texto(texto, font, color):
+    """_summary_
+
+    Args:
+        texto (_type_): _description_
+        font (_type_): _description_
+        color (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return font.render(texto, True, color)
 
 def dibujar_boton(screen, rect, texto, font, color_fondo, color_texto):
+    """_summary_
+
+    Args:
+        screen (_type_): _description_
+        rect (_type_): _description_
+        texto (_type_): _description_
+        font (_type_): _description_
+        color_fondo (_type_): _description_
+        color_texto (_type_): _description_
+    """
     pygame.draw.rect(screen, color_fondo, rect)
     texto_img = font.render(texto, True, color_texto)
     texto_rect = texto_img.get_rect(center=rect.center)
     screen.blit(texto_img, texto_rect)
 
 def dibujar_cruz(screen, rect):
+    """_summary_
+
+    Args:
+        screen (_type_): _description_
+        rect (_type_): _description_
+    """
     pygame.draw.line(screen, ROJO, rect.topleft, rect.bottomright, 3)
     pygame.draw.line(screen, ROJO, rect.topright, rect.bottomleft, 3)
 
 def dibujar_tablero(ventana, jugador, tam, filas, columnas, offset_x, offset_y):
+    """_summary_
+
+    Args:
+        ventana (_type_): _description_
+        jugador (_type_): _description_
+        tam (_type_): _description_
+        filas (_type_): _description_
+        columnas (_type_): _description_
+        offset_x (_type_): _description_
+        offset_y (_type_): _description_
+    """
     for f in range(filas):
         for c in range(columnas):
             x = offset_x + c * tam
@@ -22,31 +60,42 @@ def dibujar_tablero(ventana, jugador, tam, filas, columnas, offset_x, offset_y):
 
             valor = jugador[f][c]
 
-            # 0 = vacío
+            
             if valor == 0:
                 pygame.draw.rect(ventana, GRIS, (x, y, tam, tam))
 
-            # 1 = pintado
+            
             elif valor == 1:
                 pygame.draw.rect(ventana, NEGRO, (x, y, tam, tam))
 
-            # 2 = cruz
+            
             elif valor == 2:
                 pygame.draw.rect(ventana, BLANCO, (x, y, tam, tam))
                 pygame.draw.line(ventana, ROJO, (x, y), (x+tam, y+tam), 3)
                 pygame.draw.line(ventana, ROJO, (x+tam, y), (x, y+tam), 3)
 
-            # borde
+            
             pygame.draw.rect(ventana, NEGRO, (x, y, tam, tam), 2)
 
 def dibujar_pistas(ventana, font, pistas_filas, pistas_columnas, tam, offset_x, offset_y):
-    # PISTAS DE FILAS (izquierda)
+    """_summary_
+
+    Args:
+        ventana (_type_): _description_
+        font (_type_): _description_
+        pistas_filas (_type_): _description_
+        pistas_columnas (_type_): _description_
+        tam (_type_): _description_
+        offset_x (_type_): _description_
+        offset_y (_type_): _description_
+    """
+    
     for i, pista in enumerate(pistas_filas):
         texto = " ".join(str(n) for n in pista)
         img = font.render(texto, True, NEGRO)
         ventana.blit(img, (offset_x - 10 - img.get_width(), offset_y + i * tam + tam//3))
 
-    # PISTAS DE COLUMNAS (arriba)
+
     for j, pista in enumerate(pistas_columnas):
         texto = "\n".join(str(n) for n in pista)
         lineas = texto.split("\n")
@@ -57,3 +106,13 @@ def dibujar_pistas(ventana, font, pistas_filas, pistas_columnas, tam, offset_x, 
                 offset_x + j * tam + tam//3,
                 offset_y - (len(lineas) * img.get_height()) - 10 + idx * img.get_height()
             ))
+
+def mostrar_game_over(ventana, font):
+    ventana.fill(NEGRO)
+    texto = font.render("GAME OVER", True, ROJO)
+    rect = texto.get_rect(center=(ANCHO // 2, ALTO // 2))
+    ventana.blit(texto, rect)
+    pygame.display.update()
+    pygame.time.delay(2000)
+
+    
--- paquete/logica.py
+++ paquete/logica.py
@@ -1,7 +1,17 @@
 import os
 import random
+from paquete.config import *
+
 
 def cargar_nonograma_csv(ruta):
+    """_summary_
+
+    Args:
+        ruta (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     nonograma_resuelto = []
     with open(ruta, "r") as archivo:
         for linea in archivo:
@@ -9,7 +19,30 @@ def cargar_nonograma_csv(ruta):
             nonograma_resuelto.append(fila)
     return nonograma_resuelto
 
+def elegir_nonograma_random(ruta_categoria):
+    """_summary_
+
+    Args:
+        ruta_categoria (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    archivos = os.listdir(ruta_categoria)
+    elegido = random.choice(archivos)
+    
+    return ruta_categoria + elegido
+
 def crear_nonograma_jugador(filas, columnas):
+    """_summary_
+
+    Args:
+        filas (_type_): _description_
+        columnas (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     nonograma_jugador = []
     for i in range(filas):
         fila = []
@@ -22,6 +55,15 @@ def cambiar_estado(nonograma_jugador, fila, columna, estado):
     nonograma_jugador[fila][columna] = estado
 
 def comparar_nonogramas(jugador, resuelto):
+    """_summary_
+
+    Args:
+        jugador (_type_): _description_
+        resuelto (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     for i in range(len(resuelto)):
         for j in range(len(resuelto[i])):
             if resuelto[i][j] == 1 and jugador[i][j] != 1:
@@ -31,19 +73,44 @@ def comparar_nonogramas(jugador, resuelto):
     return True
 
 def elegir_nonograma_random(ruta_categoria):
+    """_summary_
+
+    Args:
+        ruta_categoria (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     archivos = os.listdir(ruta_categoria)
     elegido = random.choice(archivos)
     return ruta_categoria + elegido
 
 def iniciar_partida(ruta):
+    """_summary_
+
+    Args:
+        ruta (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     resuelto = cargar_nonograma_csv(ruta)
     filas = len(resuelto)
     columnas = len(resuelto[0])
+    resuelto = cargar_nonograma_csv(ruta)
     jugador = crear_nonograma_jugador(filas, columnas)
     return jugador, resuelto
 
 
 def calcular_pistas(nonograma):
+    """_summary_
+
+    Args:
+        nonograma (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     filas = len(nonograma)
     columnas = len(nonograma[0])
 
@@ -89,6 +156,24 @@ def calcular_pistas(nonograma):
         pistas_columnas.append(pistas)
 
     return pistas_filas, pistas_columnas
+
+def controlar_error(jugador, resuelto, fila, columna):
+    """_summary_
+
+    Args:
+        jugador (_type_): _description_
+        resuelto (_type_): _description_
+        fila (_type_): _description_
+        columna (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if resuelto[fila][columna] == 1 and jugador[fila][columna] == 0:
+        return True
+    if resuelto[fila][columna] == 0 and jugador[fila][columna] == 1:
+        return True
+    return False
 
 
 
